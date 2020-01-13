ede4d45da478ef9f1e351ebaaa9c0a6cc25a6c3f
manuelatrotta <manuelat88@hotmail.it>
2020-01-13 17:05:20 +0100

    richiesta parola all'utente

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,4 +1,5 @@
 alert('welcome');
 //Scrivi una funzione per capire se la parola è palindroma
 // una parola è tale se la prima è leggibile sia da destra che sinistra. La prima lettera è uguale all'ultima, la seconda alla penultima è così via
-function
+var parolaScelta = prompt('scegli una parola e verificheremo se è palindroma');
+console.log(parolaScelta);
