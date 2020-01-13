da6e7ae3d5adb8a61ecc1d1ad27bc4322f37f841
manuelatrotta <manuelat88@hotmail.it>
2020-01-13 15:53:53 +0100

    generazione numero random

--- pari_dispari/js/script.js
+++ pari_dispari/js/script.js
@@ -5,4 +5,13 @@ var sceltaNumero = parseInt(prompt('scegli ora un numero da 1 a 5'));
 console.log(sceltaUtente);
 console.log(sceltaNumero);
 //Generiamo un numero random (sempre da 1 a 5) per il computer.
+var randomNumber = getRandom(1, 5);
+console.log(randomNumber);
+function getRandom (min, max){
+  min = Math.ceil(1);
+  max = Math.floor(5);
+  var random = Math.floor(Math.random() * (5 - 1 + 1) + 1);
+  return random;
+}
+
 //Sommiamo i due numeri e dichiariamo chi ha vinto (controlliamo se la somma da pari o dispari).
