8cc38e1187a53e397ed9a12ca007e30c3fb9c233
manuelatrotta <manuelat88@hotmail.it>
2020-01-13 15:44:23 +0100

    inizio richieste utente

--- pari_dispari/js/script.js
+++ pari_dispari/js/script.js
@@ -1,1 +1,8 @@
 alert('welcome');
+//L’utente sceglie pari o dispari e un numero da 1 a 5.
+var sceltaUtente = prompt('scegli pari o dispari');
+var sceltaNumero = parseInt(prompt('scegli ora un numero da 1 a 5'));
+console.log(sceltaUtente);
+console.log(sceltaNumero);
+//Generiamo un numero random (sempre da 1 a 5) per il computer.
+//Sommiamo i due numeri e dichiariamo chi ha vinto (controlliamo se la somma da pari o dispari).
