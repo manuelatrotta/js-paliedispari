1cd3d33c157403ea04d834b3e4e144dd2e647ef4
manuelatrotta <manuelat88@hotmail.it>
2020-01-13 18:29:42 +0100

    soluzione palindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,16 +1,18 @@
 alert('welcome');
 //Scrivi una funzione per capire se la parola è palindroma
 // una parola è tale se la prima è leggibile sia da destra che sinistra. La prima lettera è uguale all'ultima, la seconda alla penultima è così via
-var parolaScelta = prompt('scegli una parola e verificheremo se è palindroma');
-console.log(parolaScelta);
-function palindromo(parolaScelta){
-  var i = parolaScelta.length;
-  while(i > 0){
-    i--;
-    if( parolaScelta[i] = parolaScelta.charAt(parolaScelta.length)-1 ){
-      console.log('la parola è palindroma');
-    }else{
-      console.log('la parola non è palindroma');
-    }
-    return risultato;
+var parolaUtente = prompt('inserisci una parola');
+var parolaGirata = rovesciaParola(parolaUtente);
+if(parolaGirata == parolaUtente){
+  console.log('è palindroma');
+}else{
+  console.log('non è palindroma');
+}
+
+function rovesciaParola(parola){
+  var parolaReverse = '';
+  for(var i = parola.length -1; i>=0; i--){
+    parolaReverse += parola[i];
+  }
+  return parolaReverse;
 }
