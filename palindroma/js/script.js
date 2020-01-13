2f9cd2d6fd83c9b2322f0099701f84f20b8fab05
manuelatrotta <manuelat88@hotmail.it>
2020-01-13 17:24:09 +0100

    tentativo risoluzione

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -3,3 +3,16 @@ alert('welcome');
 // una parola è tale se la prima è leggibile sia da destra che sinistra. La prima lettera è uguale all'ultima, la seconda alla penultima è così via
 var parolaScelta = prompt('scegli una parola e verificheremo se è palindroma');
 console.log(parolaScelta);
+var risultato(parolaScelta);
+
+function palindromo(parolaScelta){
+  var i = parolaScelta.length;
+  while(i > 0){
+    i--;
+    if( parolaScelta[i] = parolaScelta.charAt(parolaScelta.length)-1 ){
+      console.log('la parola è palindroma');
+    }else{
+      console.log('la parola non è palindroma');
+    }
+    return risultato;
+}
